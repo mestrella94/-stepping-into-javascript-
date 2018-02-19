5ef4efcf63e67ded804b056b7ecf534f9026d71c
mestrella94 <36056671+mestrella94@users.noreply.github.com>
2018-02-19 15:38:26 -0500

    Create assignment 10.js

---
+++ assignment 10.js
@@ -0,0 +1,10 @@
+function getAverage (a,b,c,d,e,f){
+    
+  var average = (a + b + c + d + e + f) / 6;
+  console.log(average);
+    
+}
+
+
+var myResult = getAverage(7,8,9,10,11,12);
+console,log("the average is" + myResult);
